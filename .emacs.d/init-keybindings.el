cf057043ac7825f9b515a7932c7079a81cc639a0
Brian Lange <thebrianlange@gmail.com>
2013-07-26 17:09:32 -0500

    Make .emacs.d mac-switch-meta only load on mac

--- .emacs.d/init-keybindings.el
+++ .emacs.d/init-keybindings.el
@@ -35,21 +35,22 @@
 (global-set-key [(hyper z)] 'undo)
  
 ;; mac switch meta key
-(defun mac-switch-meta nil 
-  "switch meta between Option and Command"
-  (interactive)
-  (if (eq mac-option-modifier nil)
-      (progn
-	(setq mac-option-modifier 'meta)
-	(setq mac-command-modifier 'hyper)
-	)
-    (progn 
-      (setq mac-option-modifier nil)
-      (setq mac-command-modifier 'meta)
+(if (eq system-type 'darwin)
+    (defun mac-switch-meta nil 
+      "switch meta between Option and Command"
+      (interactive)
+      (if (eq mac-option-modifier nil)
+          (progn
+            (setq mac-option-modifier 'meta)
+            (setq mac-command-modifier 'hyper)
+            )
+        (progn 
+          (setq mac-option-modifier nil)
+          (setq mac-command-modifier 'meta)
+          )
+        )
       )
-    )
-  )
-(mac-switch-meta)
-
+  (mac-switch-meta)
+)
 
 (provide 'init-keybindings)
